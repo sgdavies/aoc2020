a28abba7ae2a2dce2083f7db0c1b97bef264ea69
Sam Davies <samgdavies@gmail.com>
2020-12-04 19:23:15 +0000

    structure and tests for four part two

--- src/day_four.rs
+++ src/day_four.rs
@@ -21,6 +21,10 @@ impl DayFour {
     pub fn solve_one(&self) -> usize {
         self.passports.iter().filter(|x| x.complete()).count()
     }
+
+    pub fn solve_two(&self) -> usize {
+        self.passports.iter().filter(|x| x.valid()).count()
+    }
 }
 
 struct Passport {
@@ -71,6 +75,67 @@ impl Passport {
             && self.ecl.is_some()
             && self.pid.is_some()
     }
+
+    // Valid, according to all the rules
+    fn valid(&self) -> bool {
+        check_byr(self.byr.as_ref())
+            && check_iyr(self.iyr.as_ref())
+            && check_eyr(self.eyr.as_ref())
+            && check_hgt(self.hgt.as_ref())
+            && check_hcl(self.hcl.as_ref())
+            && check_ecl(self.ecl.as_ref())
+            && check_pid(self.pid.as_ref())
+    }
+}
+
+fn check_year(year: Option<&String>, earliest: u32, latest: u32) -> bool {
+    match year {
+        Some(year) => match year.parse::<u32>() {
+            Ok(val) => earliest <= val && val <= latest,
+            Err(_) => false,
+        },
+        None => false,
+    }
+}
+
+fn check_byr(byr: Option<&String>) -> bool {
+    check_year(byr, 1920, 2002)
+}
+
+fn check_iyr(iyr: Option<&String>) -> bool {
+    check_year(iyr, 2010, 2020)
+}
+
+fn check_eyr(eyr: Option<&String>) -> bool {
+    check_year(eyr, 2020, 2030)
+}
+
+fn check_hgt(hgt: Option<&String>) -> bool {
+    match hgt {
+        Some(_hgt) => false, // TODO
+        None => false,
+    }
+}
+
+fn check_hcl(hcl: Option<&String>) -> bool {
+    match hcl {
+        Some(_hcl) => false, // TODO
+        None => false,
+    }
+}
+
+fn check_ecl(ecl: Option<&String>) -> bool {
+    match ecl {
+        Some(_ecl) => false, // TODO
+        None => false,
+    }
+}
+
+fn check_pid(pid: Option<&String>) -> bool {
+    match pid {
+        Some(_pid) => false, // TODO
+        None => false,
+    }
 }
 
 #[cfg(test)]
@@ -78,8 +143,87 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test() {
+    fn test_one() {
         let four = DayFour::new("data/4a_example.txt");
         assert!(four.solve_one() == 2);
+    }
+
+    #[test]
+    fn test_two_invalid() {
+        let four = DayFour::new("data/4b_invalid.txt");
+        assert!(four.solve_two() == 0);
+    }
+
+    #[test]
+    fn test_two_valid() {
+        let four = DayFour::new("data/4b_valid.txt");
+        assert!(four.solve_two() == 4);
+    }
+
+    #[test]
+    fn test_byr() {
+        assert!(check_byr(Some(&"1920".to_string())));
+        assert!(check_byr(Some(&"2002".to_string())));
+        assert!(!check_byr(Some(&"1919".to_string())));
+        assert!(!check_byr(Some(&"2003".to_string())));
+        assert!(!check_byr(Some(&"192".to_string())));
+        assert!(!check_byr(Some(&"abc".to_string())));
+    }
+
+    #[test]
+    fn test_iyr() {
+        assert!(check_iyr(Some(&"2010".to_string())));
+        assert!(check_iyr(Some(&"2020".to_string())));
+        assert!(!check_iyr(Some(&"2009".to_string())));
+        assert!(!check_iyr(Some(&"2021".to_string())));
+    }
+
+    #[test]
+    fn test_eyr() {
+        assert!(check_eyr(Some(&"2020".to_string())));
+        assert!(check_eyr(Some(&"2030".to_string())));
+        assert!(!check_eyr(Some(&"2019".to_string())));
+        assert!(!check_eyr(Some(&"2031".to_string())));
+    }
+
+    #[test]
+    fn test_hgt() {
+        assert!(check_hgt(Some(&"60in".to_string())));
+        assert!(check_hgt(Some(&"190cm".to_string())));
+        assert!(!check_hgt(Some(&"190in".to_string())));
+        assert!(!check_hgt(Some(&"190".to_string())));
+    }
+
+    #[test]
+    fn test_hcl() {
+        assert!(check_hcl(Some(&"#123abc".to_string())));
+        assert!(!check_hcl(Some(&"#123abz".to_string())));
+        assert!(!check_hcl(Some(&"123abc".to_string())));
+    }
+
+    #[test]
+    fn test_ecl() {
+        assert!(check_ecl(Some(&"amb".to_string())));
+        assert!(check_ecl(Some(&"blu".to_string())));
+        assert!(check_ecl(Some(&"brn".to_string())));
+        assert!(check_ecl(Some(&"gry".to_string())));
+        assert!(check_ecl(Some(&"grn".to_string())));
+        assert!(check_ecl(Some(&"hzl".to_string())));
+        assert!(check_ecl(Some(&"oth".to_string())));
+
+        assert!(check_ecl(Some(&"".to_string())));
+        assert!(check_ecl(Some(&"wat".to_string())));
+    }
+
+    #[test]
+    fn test_pid() {
+        assert!(check_pid(Some(&"000000001".to_string())));
+        assert!(check_pid(Some(&"100000002".to_string())));
+
+        assert!(!check_pid(Some(&"10000002".to_string())));
+        assert!(!check_pid(Some(&"1a0000002".to_string())));
+        assert!(!check_pid(Some(&"100 00002".to_string())));
+        assert!(!check_pid(Some(&"0123456789".to_string())));
+        assert!(!check_pid(Some(&"1234567890".to_string())));
     }
 }
