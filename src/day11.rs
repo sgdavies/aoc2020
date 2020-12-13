7713c5e878b123b7560a9f0accac21e0aa61d4c2
Sam Davies <samgdavies@gmail.com>
2020-12-13 19:55:53 +0000

    day 11 part 2

--- src/day11.rs
+++ src/day11.rs
@@ -1,14 +1,21 @@
 use crate::file_to_vec;
 use std::collections::HashMap;
 
+// Day 11. Runs fine, but takes ~20s on each part (running on a
+// Raspberry Pi).  Could be worth optimising.
+
 struct Seats {
     seats: HashMap<(isize, isize), bool>,
-    dim_x: usize,
-    dim_y: usize,
+    dim_x: isize,
+    dim_y: isize,
 }
 
-pub(crate) fn solve_part_one(filename: &str) -> usize {
+pub(crate) fn part_one(filename: &str) -> usize {
     solve(filename, move_seats_one)
+}
+
+pub(crate) fn part_two(filename: &str) -> usize {
+    solve(filename, move_seats_two)
 }
 
 fn solve(filename: &str, move_seats: fn(&mut Seats) -> bool) -> usize {
@@ -32,17 +39,24 @@ fn solve(filename: &str, move_seats: fn(&mut Seats) -> bool) -> usize {
 
     let mut seats = Seats {
         seats,
-        dim_x,
-        dim_y,
+        dim_x: dim_x as isize,
+        dim_y: dim_y as isize,
     };
 
     while move_seats(&mut seats) {}
-    seats.seats.values().filter(|b| **b).count()
+    let answer = seats.seats.values().filter(|b| **b).count();
+    // println!("Iterations: {:}", answer);
+    answer
 }
 
 // Return true if there were any seat changes, otherwise false
 fn move_seats_one(seats: &mut Seats) -> bool {
     move_seats(seats, adjacent_one, 4)
+}
+
+// Return true if there were any seat changes, otherwise false
+fn move_seats_two(seats: &mut Seats) -> bool {
+    move_seats(seats, visible, 5)
 }
 
 fn move_seats(
@@ -70,6 +84,7 @@ fn move_seats(
     }
 
     seats.seats = new_seats;
+    // _pretty_print(&seats);
     changed
 }
 
@@ -96,11 +111,45 @@ fn adjacent_indices(x: isize, y: isize) -> Vec<(isize, isize)> {
     ]
 }
 
-fn _pretty_print(seats: &HashMap<(isize, isize), bool>, dim_x: usize, dim_y: usize) {
-    for y in 0..dim_y {
+fn visible(x: isize, y: isize, old_seats: &Seats) -> u32 {
+    // Returns the number of directions in which you can see an
+    // occupied chair, stopping at the first chair (occupied or empty)
+    let direction_steps = [
+        (-1, -1),
+        (0, -1),
+        (1, -1),
+        (-1, 0),
+        (1, 0),
+        (-1, 1),
+        (0, 1),
+        (1, 1),
+    ];
+    direction_steps.iter().fold(0, |acc, (sx, sy)| {
+        let mut next_x = x + sx;
+        let mut next_y = y + sy;
+        let mut seen = false;
+        while 0 <= next_x && next_x < old_seats.dim_x && 0 <= next_y && next_y < old_seats.dim_y {
+            if let Some(occupied) = old_seats.seats.get(&(next_x, next_y)) {
+                seen |= occupied;
+                break;
+            }
+
+            next_x += sx;
+            next_y += sy;
+        }
+
+        match seen {
+            true => acc + 1,
+            false => acc,
+        }
+    })
+}
+
+fn _pretty_print(seats: &Seats) {
+    for y in 0..seats.dim_y {
         let mut s: String = String::new();
-        for x in 0..dim_x {
-            s.push(match seats.get(&(x as isize, y as isize)) {
+        for x in 0..seats.dim_x {
+            s.push(match seats.seats.get(&(x, y)) {
                 Some(true) => '#',
                 Some(false) => 'L',
                 None => '.',
@@ -117,6 +166,11 @@ mod tests {
 
     #[test]
     fn test_one() {
-        assert!(solve_part_one("data/11_example.txt") == 37);
+        assert!(part_one("data/11_example.txt") == 37);
+    }
+
+    #[test]
+    fn test_two() {
+        assert!(part_two("data/11_example.txt") == 26);
     }
 }
