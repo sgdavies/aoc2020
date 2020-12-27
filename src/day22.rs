2b51507d579fe31437899af856f965185f4d30d0
Sam Davies <samgdavies@gmail.com>
2020-12-27 17:45:12 +0000

    clippy

--- src/day22.rs
+++ src/day22.rs
@@ -5,7 +5,7 @@ pub(crate) fn part_one(deck_one: &str, deck_two: &str) -> i32 {
     let mut deck_one: VecDeque<i32> = file_to_vec(deck_one).iter().map(|s| s.parse::<i32>().unwrap()).collect();
     let mut deck_two: VecDeque<i32> = file_to_vec(deck_two).iter().map(|s| s.parse::<i32>().unwrap()).collect();
 
-    while deck_one.len() > 0 && deck_two.len() > 0 {
+    while !deck_one.is_empty() && !deck_two.is_empty() {
         let one = deck_one.pop_front().unwrap();
         let two = deck_two.pop_front().unwrap();
 
