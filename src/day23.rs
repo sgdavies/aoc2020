e16e1298aec1dffb7408a3da9647a6e918738076
Sam Davies <samgdavies@gmail.com>
2021-01-08 20:13:57 +0000

    23 part 2 - change to linkedlist, but still too slow

--- src/day23.rs
+++ src/day23.rs
@@ -1,46 +1,70 @@
+use std::collections::LinkedList;
+
 pub(crate) fn part_one(input: &str, rounds: usize) -> String {
-    let cups: Vec<usize> = input.chars().map(|c| c.to_digit(10).unwrap() as usize).collect();
+    let cups: LinkedList<usize> = input
+        .chars()
+        .map(|c| c.to_digit(10).unwrap() as usize)
+        .collect();
 
-    let cups = crab_cups(cups, rounds);
+    let mut cups = crab_cups(cups, rounds);
 
-    // Rotate so 1 is at the start
-    let ix_1 = cups.iter().position(|&x| x == 1).unwrap();
-    let (last, first) = cups.split_at(ix_1);
+    // Build a list running from the first entry _after_ 1 and round again
+    while cups.front().unwrap() != &1 {
+        let not_1 = cups.pop_front().unwrap();
+        cups.push_back(not_1);
+    }
+    cups.pop_front(); // Remove the 1
     let mut out = String::new();
-    for c in first.iter() {
-        match c {
-            1 => {},
-            _ => out.push(std::char::from_digit(*c as u32, 10).unwrap()),
-        }
-    } 
-    for c in last.iter() {
+    for c in cups.iter() {
         out.push(std::char::from_digit(*c as u32, 10).unwrap());
-    } 
+    }
+
     out
 }
 
-pub(crate) fn crab_cups(mut cups: Vec<usize>, rounds: usize) -> Vec<usize> {
+pub(crate) fn part_two(input: &str, rounds: usize, n_cups: usize) -> usize {
+    let mut cups: LinkedList<usize> = input
+        .chars()
+        .map(|c| c.to_digit(10).unwrap() as usize)
+        .collect();
+    for next in cups.len()..n_cups {
+        cups.push_back(next + 1);
+    }
+
+    cups = crab_cups(cups, rounds);
+
+    // Lazily assume 1 isn't right at the end of the array
+    while cups.pop_front().unwrap() != 1 {}
+    cups.pop_front().unwrap() * cups.pop_front().unwrap()
+}
+
+pub(crate) fn crab_cups(mut cups: LinkedList<usize>, rounds: usize) -> LinkedList<usize> {
     let len = cups.len();
 
     for _i in 0..rounds {
-        let current = cups[0];
-        let (one, two, three) = (cups[1], cups[2], cups[3]);
+        let current = cups.pop_front().unwrap();
+        let one = cups.pop_front().unwrap();
+        let two = cups.pop_front().unwrap();
+        let three = cups.pop_front().unwrap();
         let destination = get_destination(current, one, two, three, len);
-        // println!("Round {:} - c {:}, [{:} {:} {:}] -> {:}", _i, current, one, two, three, destination);
 
-        let mut next_cups: Vec<usize> = Vec::new();
-        for &c in cups[4..].iter() {
-            next_cups.push(c);
-            if c == destination {
-                next_cups.push(one);
-                next_cups.push(two);
-                next_cups.push(three);
-            }
-        }
-        next_cups.push(current);
+        // Rust LinkedList doesn't have a find/index_of method :-(
+        let dest_index = cups
+            .iter()
+            .enumerate()
+            .fold(None, |a, (i, x)| match x {
+                t if *t == destination => Some(i),
+                _ => a,
+            })
+            .unwrap(); // Inefficient - always searches whole array.
 
-        cups = next_cups;
-        // println!("  next {:?}", cups);
+        let mut back_cups = cups.split_off(dest_index + 1);
+        cups.push_back(one);
+        cups.push_back(two);
+        cups.push_back(three);
+        cups.append(&mut back_cups);
+
+        cups.push_back(current);
     }
 
     cups
@@ -80,9 +104,30 @@ mod tests {
     }
 
     #[test]
-    fn test_game() {
+    fn test_one() {
         assert!(&part_one("389125467", 1) == "54673289");
         assert!(&part_one("389125467", 10) == "92658374");
         assert!(&part_one("389125467", 100) == "67384529");
+    }
+
+    #[test]
+    fn test_two() {
+        assert!(part_two("389125467", 0, 1_000_000) == 2 * 5);
+        assert!(part_two("389254671", 0, 1_000_000) == 10 * 11);
+    }
+
+    #[test]
+    fn _time() {
+        use std::time::Instant;
+        let all_turns = [3_000]; //[1_000, 3_000, 10_000, 30_000];//, 3_000, 10_000]; // Linear on n-turns
+        let all_sizes = [3_000, 10_000, 30_000, 100_000]; //[1_000, 10_000, 100_000];
+        for &turns in all_turns.iter() {
+            for &size in all_sizes.iter() {
+                let start = Instant::now();
+                let _ = part_two("389254671", turns, size); //1_000_000);
+                println!("{:} turns at {:} size\t{:}", turns, size, start.elapsed().as_secs());
+            }
+        }
+        assert!(false); // Show test output in stdout
     }
 }
